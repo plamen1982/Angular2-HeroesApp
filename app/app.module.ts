b121b99ae74cfb50363490af5202333c5dd07325
plamen1982 <plamen.hristov@ymail.com>
2017-01-10 21:49:12 -0500

    Add dashboard route and link

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent }  from './app.component'
 import { HeroesComponent } from './heroes.component'
 import { HeroDetailComponent } from './hero-detail.component'
 import { HeroService } from './hero.service'
-
+import { DashboardComponent} from './dashboard.component'
 
 
 
@@ -19,13 +19,23 @@ import { HeroService } from './hero.service'
       {
         path: 'heroes',
         component: HeroesComponent
-      }
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
     ])
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    DashboardComponent
   ],
   providers: [
     HeroService
