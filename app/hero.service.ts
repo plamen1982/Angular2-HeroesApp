82c10ed7d4885e3018744654c0ff8ee2e904f412
plamen1982 <plamen.hristov@ymail.com>
2017-01-17 21:20:27 -0500

    Added add method into the heroes.component.ts and add create method in hero.service

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -33,6 +33,14 @@ export class HeroService {
       .then(()=>hero)
       .catch(this.handleError)
   }
+  create(name: string) : Promise<Hero> {
+
+    return this.http
+    .post(this.heroesUrl, JSON.stringify({name: name}),{headers: this.headers})
+    .toPromise()
+    .then(response => response.json().data)
+    .catch(this.handleError)
+  }
   private handleError(error :any): Promise<any> {
     console.error('An error occured', error)
     return Promise.reject(error.message | error)
